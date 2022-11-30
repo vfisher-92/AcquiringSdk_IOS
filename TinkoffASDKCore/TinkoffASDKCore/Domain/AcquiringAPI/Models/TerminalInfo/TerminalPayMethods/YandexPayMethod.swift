8890cc398f5ea4ba94d161e3f1c57d179cdc91c1
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2022-11-30 12:40:07 +0300

    Add GetTerminalPayMethodsPayload

---
+++ TinkoffASDKCore/TinkoffASDKCore/Domain/AcquiringAPI/Models/TerminalInfo/TerminalPayMethods/YandexPayMethod.swift
@@ -0,0 +1,31 @@
+//
+//  YandexPayMethod.swift
+//  TinkoffASDKCore
+//
+//  Created by r.akhmadeev on 29.11.2022.
+//
+
+import Foundation
+
+/// Данные для оплаты с помощью YandexPay
+public struct YandexPayMethod {
+    /// ID продавца для создания платежной формы YandexPay (`YPCardPaymentMethod.gatewayMerchantId`)
+    public let merchantId: String
+    /// Имя продавца для инициализации YandexPay (`YandexPaySDKMerchant.name`)
+    public let merchantName: String
+    /// URL продавца для инициализации YandexPay  (`YandexPaySDKMerchant.url`)
+    public let merchantOrigin: String
+    /// ID продавца для инициализации YandexPay (`YandexPaySDKMerchant.id`)
+    public let showcaseId: String
+}
+
+// MARK: - YandexPayMethod + Decodable
+
+extension YandexPayMethod: Decodable {
+    private enum CodingKeys: String, CodingKey {
+        case merchantId = "MerchantId"
+        case merchantName = "MerchantName"
+        case merchantOrigin = "MerchantOrigin"
+        case showcaseId = "ShowcaseId"
+    }
+}
