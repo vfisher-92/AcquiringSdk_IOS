35c1b20c4d2e538dc70d232c3da4839e67e7bbc8
Ruslan Akhmadeev <r.akhmadeev@tinkoff.ru>
2022-12-16 12:21:40 +0300

    Update animation for PaymentActivityView

--- TinkoffASDKUI/TinkoffASDKUI/Modules/PaymentActivity/Views/PaymentActivityView.swift
+++ TinkoffASDKUI/TinkoffASDKUI/Modules/PaymentActivity/Views/PaymentActivityView.swift
@@ -29,6 +29,13 @@ final class PaymentActivityView: UIView {
     private lazy var processingView = PaymentActivityProcessingView()
     private lazy var processedView = PaymentActivityProcessedView(delegate: self)
 
+    private lazy var overlayView: UIView = {
+        let view = PassthroughView()
+        view.backgroundColor = ASDKColors.Background.elevation1.color
+        view.alpha = .zero
+        return view
+    }()
+
     // MARK: Constraints
 
     private lazy var processingViewConstraints: [NSLayoutConstraint] = [
@@ -64,43 +71,81 @@ final class PaymentActivityView: UIView {
         fatalError("init(coder:) has not been implemented")
     }
 
+    // MARK: Parent Methods
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        overlayView.frame = bounds
+    }
+
     // MARK: View Updating
 
     func update(with state: PaymentActivityViewState, animated: Bool = true) {
         guard self.state != state else { return }
+
         let oldState = self.state
         self.state = state
 
-        switch state {
-        case .idle:
-            processingView.isHidden = true
-            NSLayoutConstraint.deactivate(processingViewConstraints)
-            processedView.isHidden = true
-            NSLayoutConstraint.deactivate(processedViewConstraints)
-        case let .processing(state):
-            processingView.isHidden = false
-            NSLayoutConstraint.activate(processingViewConstraints)
-            processingView.update(with: state)
-            processedView.isHidden = true
-            NSLayoutConstraint.deactivate(processedViewConstraints)
-        case let .processed(state):
-            processingView.isHidden = true
-            NSLayoutConstraint.deactivate(processingViewConstraints)
-            processedView.isHidden = false
-            NSLayoutConstraint.activate(processedViewConstraints)
-            processedView.update(with: state)
-        }
+        showOverlay { [self] _ in
+            switch state {
+            case .idle:
+                NSLayoutConstraint.deactivate(processingViewConstraints)
+                processingView.removeFromSuperview()
 
-        delegate?.paymentActivityView(self, didChangeStateFrom: oldState, to: state)
+                NSLayoutConstraint.deactivate(processedViewConstraints)
+                processedView.removeFromSuperview()
+            case let .processing(state):
+                processingView.update(with: state)
+                insertSubview(processingView, belowSubview: overlayView)
+                NSLayoutConstraint.activate(processingViewConstraints)
+
+                NSLayoutConstraint.deactivate(processedViewConstraints)
+                processedView.removeFromSuperview()
+            case let .processed(state):
+                NSLayoutConstraint.deactivate(processingViewConstraints)
+                processingView.removeFromSuperview()
+
+                processedView.update(with: state)
+                insertSubview(processedView, belowSubview: overlayView)
+                NSLayoutConstraint.activate(processedViewConstraints)
+            }
+
+            DispatchQueue.main.async {
+                self.delegate?.paymentActivityView(self, didChangeStateFrom: oldState, to: state)
+            }
+
+            hideOverlay()
+        }
     }
 
     // MARK: Initial Configuration
 
     private func setupView() {
-        addSubview(processingView)
         processingView.translatesAutoresizingMaskIntoConstraints = false
-        addSubview(processedView)
         processedView.translatesAutoresizingMaskIntoConstraints = false
+        addSubview(overlayView)
+    }
+
+    // MARK: - Overlay Animation
+
+    private func showOverlay(completion: ((Bool) -> Void)? = nil) {
+        UIView.animate(
+            withDuration: .animationDuration,
+            delay: .zero,
+            options: [.curveEaseIn],
+            animations: { self.overlayView.alpha = 1 },
+            completion: completion
+        )
+    }
+
+    private func hideOverlay(completion: ((Bool) -> Void)? = nil) {
+        UIView.animate(
+            withDuration: .animationDuration,
+            delay: .zero,
+            options: [.curveEaseOut],
+            animations: { self.overlayView.alpha = .zero },
+            completion: completion
+        )
     }
 }
 
@@ -131,4 +176,8 @@ private extension CGFloat {
     static let commonBottomInset: CGFloat = 24
     static let processedHorizontalInset: CGFloat = 16
     static let processingHorizontalInset: CGFloat = 23.5
+}
+
+private extension TimeInterval {
+    static let animationDuration: TimeInterval = 0.4
 }
